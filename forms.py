da3cc2ac136eccda470ecd84a79bb2964d5c35ff
Gustavo Sene <gustavodev6331@gmail.com>
2026-07-05 14:52:16 -0400

    created a login form

--- forms.py
+++ forms.py
@@ -21,6 +21,9 @@ class RegisterForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     submit = SubmitField("Submit")
 # TODO: Create a LoginForm to login existing users
-
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # TODO: Create a CommentForm so users can leave comments below posts
